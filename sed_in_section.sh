119065c12d51004efc4b563b1beebc63d3e827dc
kowwwalski <flazh@yandex.ru>
2016-11-22 20:34:07 +0300

    Add files via upload

---
+++ sed_in_section.sh
@@ -0,0 +1,11 @@
+﻿echo "Input your var: "
+read var
+sleep 0.5s
+#
+if test -n "$var"
+then
+#edit only the one section
+  cat inputfile | sed -e "/section_start/,/section_end/ s/template/$var/" > outputfile
+else
+  echo "No input, pass the step"
+fi
---
+++ simple_sed.sh
@@ -0,0 +1,10 @@
+﻿echo "Input your var: "
+read var
+sleep 0.5s
+#
+if test -n "$var"
+then
+  cat inputfile | sed s/"template"/"$var"/g > outputfile
+else
+  echo "No input, pass the step"
+fi
