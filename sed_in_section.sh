b86bad91e0681a865ff2a9b9c78342f8e2056335
Maxim Chinenkof <flazh@yandex.ru>
2018-01-11 20:35:44 +0300

    Update sed_in_section.sh

--- sed_in_section.sh
+++ sed_in_section.sh
@@ -1,11 +1,11 @@
-﻿echo "Input your var: "
+﻿echo "Input your new var: "
 read var
 sleep 0.5s
 #
 if test -n "$var"
 then
-#edit only the one section
-  cat inputfile | sed -e "/section_start/,/section_end/ s/template/$var/" > outputfile
+#edit only one section, where %template% is old variable value
+  cat %inputfile% | sed -e "/%section_start%/,/%section_end%/ s/%template%/$var/" > outputfile
 else
   echo "No input, pass the step"
 fi
