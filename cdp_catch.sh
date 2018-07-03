6841db4ce6027534e44c69b370195b6cdc3003bb
Maxim Chinenkof <flazh@yandex.ru>
2018-07-03 12:45:39 +0300

    Update cdp_catch.sh
    interactive request for iface-name and report just device&port

--- cdp_catch.sh
+++ cdp_catch.sh
@@ -1,1 +1,5 @@
-tcpdump -nn -v -i eth0 -s 1500 -c 1 'ether[20:2] == 0x2000'
+#!/bin/bash
+echo "Set if name:"
+read en
+
+tcpdump -nn -v -i $en -s 1500 -c 1 'ether[20:2] == 0x2000' | grep -E 'Device-ID|Port-ID'
