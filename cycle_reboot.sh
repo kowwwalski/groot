342a1cc3002d259b321c29d771809a1606d6bcf9
Maxim Chinenkof <flazh@yandex.ru>
2019-11-08 17:48:08 +0300

    Create cycle_reboot.sh

---
+++ cycle_reboot.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#reboots printers (HP mostly) from list every 30min 10 times
+for (( i = 1; i < 10; i++ ))
+do
+cat /path/to/hostlist | while read host
+do
+	snmpset -v 1 -c public $host ".1.3.6.1.2.1.43.5.1.1.3.1" i 4
+sleep 1800
+done
+done
