e80647a1ebde32f66229f1f5512bd108c1a71ee2
Maxim Chinenkof <flazh@yandex.ru>
2020-07-27 10:42:02 +0300

    Create add_printer_lpadmin.sh

---
+++ add_printer_lpadmin.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#set variables
+dir=$(dirname "$0")
+prnname= #set desired printer display name
+prnip= #set printer ip-address
+prnloc= #set desired display location
+#
+#check installed printers in system and install if not exists
+if [[ $(lpstat -p | awk '{print $2}' | grep -i "$prnname") ]]; then
+    echo "Printer $prnname already installed. Check System Preferences > Printers & Scanners"
+else
+    echo "Printer $prnname not found. Installing..."
+    # here we have ppd-like install, set path to ppd file
+    lpadmin -p $prnname -L "$prnloc" -E -v socket://$prnip -P "$dir/path/to/PPD" -o printer-is-shared=false 2>/dev/null
+    echo "Done!"
+fi
