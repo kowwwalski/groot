c8f3774166d5c5e71bc968f6a936504327578c2f
Maxim Chinenkof <flazh@yandex.ru>
2021-03-03 17:07:22 +0300

    Delete run-it-all.sh

--- run-it-all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#just for non-interactive scripts
-dir=$(dirname "$0")
-shopt -s dotglob
-#goto each folder and run .sh scripts
-find $dir/* -prune -type d | while IFS= read -r d; do
-    script=$(ls $d | grep -i ".*.sh")
-    (cd $d && exec ./$script)
-done
